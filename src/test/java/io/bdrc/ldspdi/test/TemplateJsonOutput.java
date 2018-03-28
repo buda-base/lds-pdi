981248e1b4abe96519fa1969b43051dcd5b85699
MarcAgate <agate.marc@gmail.com>
2018-03-28 14:39:57 +0200

    Added Test Units for blank nodes and Literal (case 2)

--- src/test/java/io/bdrc/ldspdi/test/TemplateJsonOutput.java
+++ src/test/java/io/bdrc/ldspdi/test/TemplateJsonOutput.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.apache.jena.fuseki.embedded.FusekiServer;
@@ -75,10 +77,23 @@ public class TemplateJsonOutput {
             "  Filter(strlen(str(?lccn))>0)\n" + 
             "}";
     
+    String literal2=prefixes+"select ?Work_Name\n" + 
+            "where {\n" + 
+            "?Work_ID a :Work.\n" + 
+            "?Work_ID skos:prefLabel ?Work_Name\n" + 
+            "  Filter(contains(?Work_Name,\"chos dbyings\"))\n" + 
+            "}";
+    
     String literal3=prefixes+" select ?s ?ct\n" + 
             "where {\n" + 
             "  ?s bdo:imageCount ?ct .\n" + 
             "  Filter(strlen(str(?ct))>0)\n" + 
+            "}";
+    
+    String blank=prefixes+"select ?Work_Title\n" + 
+            "where {\n" + 
+            "?Work_ID a :Work.\n" + 
+            "?Work_ID bdo:workTitle ?Work_Title\n" +              
             "}";
     
     static String fusekiUrl;
@@ -203,6 +218,24 @@ public class TemplateJsonOutput {
     }
     
     @Test
+    public void testJsonLiteral2() throws IOException {
+        ResultSetWrapper rsw=getResults(literal2+" limit "+limit, fusekiUrl);
+        FusekiResultSet frs=new FusekiResultSet(rsw);
+        ObjectMapper mapper = new ObjectMapper();
+        String json1=mapper.writerWithDefaultPrettyPrinter().writeValueAsString(frs);
+        
+        ResultSet rs2=QueryExecutionFactory.sparqlService(fusekiUrl,QueryFactory.create(literal2+" limit "+limit)).execSelect(); 
+        ByteArrayOutputStream baos=new ByteArrayOutputStream();
+        ResultSetFormatter.outputAsJSON(baos, rs2);
+        String json2=new String(baos.toByteArray());
+        
+        JsonNode node1=mapper.readTree(json1);
+        JsonNode node2=mapper.readTree(json2);
+        baos.close();
+        assertEquals(node1, node2);
+    }
+    
+    @Test
     public void testJsonLiteral1() throws IOException {
         ResultSetWrapper rsw=getResults(literal1+" limit "+limit, fusekiUrl);
         FusekiResultSet frs=new FusekiResultSet(rsw);
@@ -218,6 +251,30 @@ public class TemplateJsonOutput {
         JsonNode node2=mapper.readTree(json2);
         baos.close();
         assertEquals(node1, node2);
+    }
+    
+    @Test
+    public void testJsonBlank() throws IOException {
+        ResultSetWrapper rsw=getResults(blank+" limit "+limit, fusekiUrl);
+        FusekiResultSet frs=new FusekiResultSet(rsw);
+        ObjectMapper mapper = new ObjectMapper();
+        String json1=mapper.writerWithDefaultPrettyPrinter().writeValueAsString(frs);
+        System.out.println(json1);
+        ResultSet rs2=QueryExecutionFactory.sparqlService(fusekiUrl,QueryFactory.create(blank+" limit "+limit)).execSelect(); 
+        ByteArrayOutputStream baos=new ByteArrayOutputStream();
+        ResultSetFormatter.outputAsJSON(baos, rs2);
+        String json2=new String(baos.toByteArray());
+        System.out.println(json2);
+        baos.close();
+        JsonNode node1=mapper.readTree(json1);
+        JsonNode node2=mapper.readTree(json2);
+        ArrayList<String> f1=new ArrayList<>();
+        ArrayList<String> f2=new ArrayList<>();
+        Iterator<String> it1=node1.fieldNames();
+        Iterator<String> it2=node2.fieldNames();
+        while(it1.hasNext()) {f1.add(it1.next());};
+        while(it2.hasNext()) {f2.add(it2.next());};
+        assertEquals(f1,f2);
     }
     
     public static ResultSetWrapper getResults(String query, String fuseki) {
