45b8b181bbd09b6191652b170d5a1d53b8e84449
MarcAgate <agate.marc@gmail.com>
2018-04-22 00:39:14 +0200

    Cleanup + new json structure in WorkResults

--- src/main/java/io/bdrc/ldspdi/results/library/WorkResults.java
+++ src/main/java/io/bdrc/ldspdi/results/library/WorkResults.java
@@ -1,5 +1,6 @@
 package io.bdrc.ldspdi.results.library;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.jena.graph.Node;
@@ -11,6 +12,7 @@ import org.apache.jena.rdf.model.StmtIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.bdrc.ldspdi.results.Field;
 import io.bdrc.ldspdi.results.LiteralStringField;
 import io.bdrc.taxonomy.TaxModel;
 
@@ -28,7 +30,7 @@ public class WorkResults {
         
     public final static Logger log=LoggerFactory.getLogger(WorkResults.class.getName());    
      
-    public static HashMap<String,Object> getResultsMap(Model mod){
+    /*public static HashMap<String,Object> getResultsMap(Model mod){
         HashMap<String,Object> res=new HashMap<>();
         HashMap<String,HashMap<String,Integer>> count=new HashMap<>();
         HashMap<String,Integer> access=new HashMap<>();
@@ -142,6 +144,99 @@ public class WorkResults {
             map.put(uri, wm);
         }
         res.put("data",map);
+        count.put("access", access);
+        count.put("license",license);
+        count.put("status",status);
+        count.put("langScript",langScript);
+        count.put("taxonomies",tax);
+        count.put("topics",topics);
+        res.put("metadata",count);
+        return res;
+    }*/
+    
+    public static HashMap<String,Object> getResultsMap(Model mod){
+        HashMap<String,Object> res=new HashMap<>();
+        HashMap<String,HashMap<String,Integer>> count=new HashMap<>();
+        HashMap<String,Integer> access=new HashMap<>();
+        HashMap<String,Integer> license=new HashMap<>();
+        HashMap<String,Integer> status=new HashMap<>();
+        HashMap<String,Integer> langScript=new HashMap<>();
+        HashMap<String,Integer> tax=new HashMap<>();
+        HashMap<String,Integer> topics=new HashMap<>();        
+        HashMap<String,ArrayList<Field>> works=new HashMap<>();
+        ArrayList<String> tops;
+        StmtIterator iter=mod.listStatements();
+        while(iter.hasNext()) {
+            Statement st=iter.next();           
+            String uri=st.getSubject().getURI();            
+            ArrayList<Field> w=works.get(uri);
+                     
+            if(w == null) {
+                w=new ArrayList<Field>();
+            }
+            if(st.getObject().isLiteral()) {
+                w.add(new LiteralStringField(st.getPredicate().getURI(),st.getObject().asLiteral().getLanguage(),st.getObject().asLiteral().getValue().toString()));  
+            }else {
+                w.add(new Field(st.getPredicate().getURI(),st.getObject().toString()));
+            }
+            if(st.getPredicate().getURI().equals(ACCESS)) {
+                Integer ct=access.get(st.getObject().asNode().getURI());
+                if(ct!=null) {
+                    access.put(st.getObject().asNode().getURI(), ct.intValue()+1);
+                }
+                else {
+                    access.put(st.getObject().asNode().getURI(), 1);
+                }
+            }
+            if(st.getPredicate().getURI().equals(LICENSE)) {
+                Integer ct=license.get(st.getObject().asNode().getURI());
+                if(ct!=null) {
+                    license.put(st.getObject().asNode().getURI(), ct.intValue()+1);
+                }
+                else {
+                    license.put(st.getObject().asNode().getURI(), 1);
+                }
+            }
+            if(st.getPredicate().getURI().equals(LANG_SCRIPT)) {
+                Integer ct=langScript.get(st.getObject().asNode().getURI());
+                if(ct!=null) {
+                    langScript.put(st.getObject().asNode().getURI(), ct.intValue()+1);
+                }
+                else {
+                    langScript.put(st.getObject().asNode().getURI(), 1);
+                }
+            }
+            if(st.getPredicate().getURI().equals(STATUS)) {
+                Integer ct=status.get(st.getObject().asNode().getURI());
+                if(ct!=null) {
+                    status.put(st.getObject().asNode().getURI(), ct.intValue()+1);
+                }
+                else {
+                    status.put(st.getObject().asNode().getURI(), 1);
+                }
+            }
+            if(st.getPredicate().getURI().equals(WORK_GENRE)) {                                
+                String it=TaxModel.getTaxonomyItem(st.getObject().asNode().getURI());
+                if(it !=null) {                    
+                    Integer ct=tax.get(it);
+                    if(ct!=null) {
+                        tax.put(it, ct.intValue()+1);
+                    }
+                    else {
+                        tax.put(it, 1);
+                    }                    
+                }
+                Integer t=topics.get(st.getObject().asNode().getURI());
+                if(t!=null) {
+                    topics.put(st.getObject().asNode().getURI(), t.intValue()+1);
+                }
+                else {
+                    topics.put(st.getObject().asNode().getURI(), 1);
+                }
+            }
+            works.put(uri, w);
+        }
+        res.put("data",works);
         count.put("access", access);
         count.put("license",license);
         count.put("status",status);
