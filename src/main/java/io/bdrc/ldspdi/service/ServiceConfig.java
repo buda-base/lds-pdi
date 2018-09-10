2b5615aa205a5c07dcd4abcf1a82f53e0b6c5cf8
MarcAgate <agate.marc@gmail.com>
2018-09-10 17:13:58 +0200

    Specified lds-queries path for test units in ServiceConfig

--- src/main/java/io/bdrc/ldspdi/service/ServiceConfig.java
+++ src/main/java/io/bdrc/ldspdi/service/ServiceConfig.java
@@ -2,19 +2,19 @@ package io.bdrc.ldspdi.service;
 
 /*******************************************************************************
  * Copyright (c) 2018 Buddhist Digital Resource Center (BDRC)
- * 
- * If this file is a derivation of another work the license header will appear below; 
- * otherwise, this work is licensed under the Apache License, Version 2.0 
+ *
+ * If this file is a derivation of another work the license header will appear below;
+ * otherwise, this work is licensed under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with the License.
- * 
+ *
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * 
+ *
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
@@ -30,56 +30,57 @@ import org.slf4j.LoggerFactory;
 
 
 public class ServiceConfig {
-    
-    static Properties prop = new Properties();  
+
+    static Properties prop = new Properties();
     public static HashMap<String,String> params;
     public final static String FUSEKI_URL="fusekiUrl";
     public final static Logger log=LoggerFactory.getLogger(ServiceConfig.class.getName());
-    
+
     public static void init(HashMap<String,String> params) {
         ServiceConfig.params=params;
-        
+
         try {
             InputStream input = ServiceConfig.class.getClassLoader().getResourceAsStream("ldspdi.properties");
             // load a properties file
             prop.load(input);
             input.close();
-            /** 
-             * sets the PROD values of fuseki and queryPath properties 
-             * Overrides test queryPath value            * 
+            /**
+             * sets the PROD values of fuseki and queryPath properties
+             * Overrides test queryPath value            *
             **/
             Set<String> set=params.keySet();
             for(String st:set) {
                 prop.setProperty(st, params.get(st));
-            } 
+            }
             InputStream in = ServiceConfig.class.getClassLoader().getResourceAsStream("taxTreeContext.jsonld");
             in.close();
-            
+
         } catch (IOException ex) {
             log.error("ServiceConfig init error", ex);
             ex.printStackTrace();
         }
     }
-    
+
     public static void initForTests(String fusekiUrl) {
         try {
             InputStream input = ServiceConfig.class.getClassLoader().getResourceAsStream("ldspdi.properties");
             // load a properties file
             prop.load(input);
+            prop.setProperty("queryPath", "src/test/resources/arq/");
             input.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
         prop.setProperty(FUSEKI_URL, fusekiUrl);
     }
-    
+
     public static String getProperty(String key){
         return prop.getProperty(key);
     }
-    
+
     public static String getRobots() {
         return "User-agent: *"+System.lineSeparator()+"Disallow: /";
     }
-    
-    
+
+
 }
