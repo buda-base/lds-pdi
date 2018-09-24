6def8a2139351d79add853558f3df7ffd9d3f4dd
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-09-24 09:50:38 +0200

    adjust subcollection queries

--- src/test/java/io/bdrc/ldspdi/test/annotations/AnnotationCollectionTest.java
+++ src/test/java/io/bdrc/ldspdi/test/annotations/AnnotationCollectionTest.java
@@ -83,10 +83,10 @@ public class AnnotationCollectionTest extends JerseyTest {
 
     @Test
     public void subCollection() throws JsonProcessingException, IOException {
-        final Response res = target("/anncollection/ANCVOL1/sub/pages/1")
+        final Response res = target("/anncollection/ANCVOL1/sub/pages/1-3")
                 .request()
                 .header("Accept", MediaTypeUtils.MT_JSONLD)
-                .header("Prefer", AnnotationEndpoint.LDP_PMC)
+                .header("Prefer", AnnotationEndpoint.LDP_PCD)
                 .get();
         System.out.println("result:");
         System.out.println(res.getStatus());
