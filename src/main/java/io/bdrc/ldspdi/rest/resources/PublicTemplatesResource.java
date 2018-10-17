0c586e4bb92c69e6504b5dba9615d2af17997a3a
MarcAgate <agate.marc@gmail.com>
2018-10-17 23:28:45 +0200

    Revert "Clearing results cache on queries update"
    This reverts commit 99b7cc4a426c9ae2ff796962deb4e65f641bb29c.

--- src/main/java/io/bdrc/ldspdi/rest/resources/PublicTemplatesResource.java
+++ src/main/java/io/bdrc/ldspdi/rest/resources/PublicTemplatesResource.java
@@ -49,7 +49,6 @@ import io.bdrc.ldspdi.rest.features.JerseyCacheControl;
 import io.bdrc.ldspdi.results.ResultPage;
 import io.bdrc.ldspdi.results.ResultSetWrapper;
 import io.bdrc.ldspdi.results.Results;
-import io.bdrc.ldspdi.results.ResultsCache;
 import io.bdrc.ldspdi.service.GitService;
 import io.bdrc.ldspdi.sparql.LdsQuery;
 import io.bdrc.ldspdi.sparql.LdsQueryService;
@@ -241,7 +240,6 @@ public class PublicTemplatesResource {
         t.start();
         Prefixes.loadPrefixes();
         LdsQueryService.clearCache();
-        ResultsCache.clearCache();
         return Response.ok().build();
     }
 
--- src/main/java/io/bdrc/ldspdi/results/ResultsCache.java
+++ src/main/java/io/bdrc/ldspdi/results/ResultsCache.java
@@ -2,19 +2,19 @@ package io.bdrc.ldspdi.results;
 
 /*******************************************************************************
  * Copyright (c) 2018 Buddhist Digital Resource Center (BDRC)
- *
- * If this file is a derivation of another work the license header will appear below;
- * otherwise, this work is licensed under the Apache License, Version 2.0
+ * 
+ * If this file is a derivation of another work the license header will appear below; 
+ * otherwise, this work is licensed under the Apache License, Version 2.0 
  * (the "License"); you may not use this file except in compliance with the License.
- *
+ * 
  * You may obtain a copy of the License at
- *
+ * 
  *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
+ * 
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
@@ -26,6 +26,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ResultsCache {
+<<<<<<< HEAD
 
     static CacheAccess<Integer,Object> CACHE;
     public final static Logger log=LoggerFactory.getLogger(ResultsCache.class.getName());
@@ -39,20 +40,23 @@ public class ResultsCache {
     }
 
     public static void addToCache(Object res, int hash) {
+=======
+    
+    protected static CacheAccess<Integer,Object> CACHE = JCS.getInstance( "default" );
+    public final static Logger log=LoggerFactory.getLogger(ResultsCache.class.getName());
+        
+    public static void addToCache(Object res, int hash) {        
+>>>>>>> parent of 99b7cc4... Clearing results cache on queries update
         try{
-            CACHE.put(Integer.valueOf(hash), res );
+            CACHE.put(Integer.valueOf(hash), res );            
             res=null;
         }
         catch (CacheException e ){
             log.error("Problem putting Results -->"+res+" in the cache, for key -->"+hash+ " Exception:"+e.getMessage());
         }
     }
-
+    
     public static Object getObjectFromCache(int hash) {
         return CACHE.get( Integer.valueOf(hash));
-    }
-
-    public static void clearCache() {
-        CACHE.clear();
     }
 }
