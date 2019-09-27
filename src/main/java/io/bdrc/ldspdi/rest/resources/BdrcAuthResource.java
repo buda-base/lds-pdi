84748be8e6db3a81310e81a9445db47ff8639553
Marc Agate <agate.marc@gmail.com>
2019-09-27 06:31:38 -0400

    Refactoring BdrcAuthResource

--- src/main/java/io/bdrc/ldspdi/rest/resources/BdrcAuthResource.java
+++ src/main/java/io/bdrc/ldspdi/rest/resources/BdrcAuthResource.java
@@ -5,105 +5,93 @@ import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-
 import org.apache.jena.rdf.model.Model;
-import org.glassfish.jersey.server.mvc.Viewable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import io.bdrc.auth.rdf.RdfAuthModel;
 import io.bdrc.ldspdi.service.ServiceConfig;
 import io.bdrc.ldspdi.sparql.QueryProcessor;
-import io.bdrc.ldspdi.utils.MediaTypeUtils;
-import io.bdrc.ldspdi.utils.ResponseOutputStream;
+import io.bdrc.ldspdi.utils.BudaMediaTypes;
+import io.bdrc.ldspdi.utils.Helpers;
 import io.bdrc.restapi.exceptions.ErrorMessage;
 import io.bdrc.restapi.exceptions.LdsError;
 import io.bdrc.restapi.exceptions.RestException;
 
-@Component
-@Path("/")
+@RestController
+@RequestMapping("/")
 public class BdrcAuthResource {
 
-	public final static Logger log = LoggerFactory.getLogger(BdrcAuthResource.class.getName());
-	public static final String PATTERN_ASCTIME = "EEE MMM d HH:mm:ss yyyy";
-	public String fusekiUrl = ServiceConfig.getProperty(ServiceConfig.FUSEKI_URL);
+    public final static Logger log = LoggerFactory.getLogger(BdrcAuthResource.class.getName());
+    public static final String PATTERN_ASCTIME = "EEE MMM d HH:mm:ss yyyy";
+    public String fusekiUrl = ServiceConfig.getProperty(ServiceConfig.FUSEKI_URL);
 
-	@GET
-	@Path("/auth/details")
-	public Response getUsers() {
-		log.info("Call getUsers()");
-		ResponseBuilder builder = Response.ok(new Viewable("/authDetails.jsp"));
-		builder = setLastModified(builder);
-		return builder.build();
-	}
+    @GetMapping(value = "/auth/details", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getUsers() {
+        log.info("Call getUsers()");
+        ModelAndView model = new ModelAndView();
+        model.setViewName("authDetails");
+        return model;
+    }
 
-	@GET
-	@Path("/resource-auth/{res}")
-	public Response getAuthResource(@PathParam("res") final String res) throws RestException {
-		log.info("Call getAuthResource()");
-		String query = "describe <http://purl.bdrc.io/resource-auth/" + res + ">";
-		Model m = QueryProcessor.getGraphFromModel(query, QueryProcessor.getAuthGraph(null, "authDataGraph"));
-		if (m.size() == 0) {
-			LdsError lds = new LdsError(LdsError.MISSING_RES_ERR).setContext(res);
-			return Response.status(404).entity(ResponseOutputStream.getExceptionStream(ErrorMessage.getErrorMessage(404, lds))).type(MediaType.APPLICATION_JSON).build();
-		}
-		ResponseBuilder builder = Response.ok(ResponseOutputStream.getModelStream(m, "ttl"), MediaTypeUtils.getMimeFromExtension("ttl"));
-		builder = setLastModified(builder);
-		return builder.build();
-	}
+    @GetMapping(value = "/resource-auth/{res}")
+    public ResponseEntity<StreamingResponseBody> getAuthResource(@PathVariable("res") final String res) throws RestException {
+        log.info("Call getAuthResource()");
+        String query = "describe <http://purl.bdrc.io/resource-auth/" + res + ">";
+        Model m = QueryProcessor.getGraphFromModel(query, QueryProcessor.getAuthGraph(null, "authDataGraph"));
+        if (m.size() == 0) {
+            LdsError lds = new LdsError(LdsError.MISSING_RES_ERR).setContext(res);
+            return ResponseEntity.status(404).contentType(MediaType.APPLICATION_JSON).body(Helpers.getJsonObjectStream((ErrorMessage) ErrorMessage.getErrorMessage(404, lds)));
+        }
+        Calendar cal = Calendar.getInstance();
+        if (ServiceConfig.useAuth()) {
+            cal.setTimeInMillis(RdfAuthModel.getUpdated());
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat(PATTERN_ASCTIME, Locale.US);
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return ResponseEntity.ok().header("Last-Modified", formatter.format(cal.getTime())).contentType(BudaMediaTypes.getMimeFromExtension("ttl")).body(Helpers.getModelStream(m, "ttl"));
+    }
 
-	@GET
-	@Path("/authmodel")
-	public Response getAuthModel(@Context Request request) throws RestException {
-		log.info("Call to getAuthModel()");
-		ResponseBuilder builder = Response.ok(ResponseOutputStream.getModelStream(QueryProcessor.getAuthGraph(fusekiUrl, "authDataGraph")), MediaTypeUtils.getMimeFromExtension("ttl"));
-		builder = setLastModified(builder);
-		return builder.build();
-	}
+    @GetMapping(value = "/authmodel")
+    public ResponseEntity<StreamingResponseBody> getAuthModel() throws RestException {
+        log.info("Call to getAuthModel()");
+        Calendar cal = Calendar.getInstance();
+        if (ServiceConfig.useAuth()) {
+            cal.setTimeInMillis(RdfAuthModel.getUpdated());
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat(PATTERN_ASCTIME, Locale.US);
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return ResponseEntity.ok().header("Last-Modified", formatter.format(cal.getTime())).contentType(BudaMediaTypes.getMimeFromExtension("ttl")).body(Helpers.getModelStream(QueryProcessor.getAuthGraph(fusekiUrl, "authDataGraph")));
+    }
 
-	@GET
-	@Path("/authmodel/updated")
-	public long getAuthModelUpdated(@Context Request request) {
-		// log.info("Call to getAuthModelUpdated()");
-		if (ServiceConfig.useAuth()) {
-			return RdfAuthModel.getUpdated();
-		} else {
-			return 999999999;
-		}
-	}
+    @GetMapping(value = "/authmodel/updated")
+    public long getAuthModelUpdated() {
+        log.info("Call to getAuthModelUpdated()");
+        if (ServiceConfig.useAuth()) {
+            return RdfAuthModel.getUpdated();
+        } else {
+            return 999999999;
+        }
+    }
 
-	@POST
-	@Path("/callbacks/github/bdrc-auth")
-	// @Consumes(MediaType.APPLICATION_JSON)
-	public Response updateAuthModel() throws RestException {
-		// if (ServiceConfig.useAuth()) {
-		System.out.println("updating Auth data model() >>");
-		log.info("updating Auth data model() >>");
-		Thread t = new Thread(new RdfAuthModel());
-		t.start();
-		return Response.ok("Auth Model was updated").build();
-		// }
-		// return Response.ok("Auth usage is disabled").build();
-	}
+    @GetMapping(value = "/callbacks/github/bdrc-auth")
+    public ResponseEntity<String> updateAuthModel() throws RestException {
+        // if (ServiceConfig.useAuth()) {
+        System.out.println("updating Auth data model() >>");
+        log.info("updating Auth data model() >>");
+        Thread t = new Thread(new RdfAuthModel());
+        t.start();
+        return ResponseEntity.ok("Auth Model was updated");
+        // }
+        // return Response.ok("Auth usage is disabled").build();
+    }
 
-	private ResponseBuilder setLastModified(ResponseBuilder builder) {
-		Calendar cal = Calendar.getInstance();
-		if (ServiceConfig.useAuth()) {
-			cal.setTimeInMillis(RdfAuthModel.getUpdated());
-		}
-		SimpleDateFormat formatter = new SimpleDateFormat(PATTERN_ASCTIME, Locale.US);
-		formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
-		builder.header("Last-Modified", formatter.format(cal.getTime()));
-		return builder;
-	}
 }
