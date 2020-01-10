5bdd8086fc9d5214995dcbb4eca1021b1d29a28c
eroux <elie.roux@telecom-bretagne.eu>
2020-01-10 11:25:51 +0100

    add placeGraph

--- src/main/java/io/bdrc/ldspdi/results/library/TypeResults.java
+++ src/main/java/io/bdrc/ldspdi/results/library/TypeResults.java
@@ -6,22 +6,20 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
-import org.apache.jena.vocabulary.RDF;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.bdrc.ldspdi.exceptions.RestException;
 import io.bdrc.ldspdi.results.Field;
 
-public class WorkInstanceResults {
+public class TypeResults {
 
-    public final static Logger log = LoggerFactory.getLogger(WorkInstanceResults.class);
+    public final static Logger log = LoggerFactory.getLogger(TypeResults.class);
 
-    public static Map<String, Object> getResultsMap(Model mod) throws RestException {
+    public static Map<String, Object> getResultsMap(final Model mod) throws RestException {
         long start = 0;
         if (log.isDebugEnabled())
             start = System.nanoTime();
@@ -29,17 +27,16 @@ public class WorkInstanceResults {
         Map<String, ArrayList<Field>> main = new HashMap<>();
         Map<String, ArrayList<Field>> aux = new HashMap<>();
         Map<Resource, Boolean> isInstance = new HashMap<>();
-        //ResIterator workit = mod.listResourcesWithProperty(RDF.type, mod.getResource("http://purl.bdrc.io/ontology/core/Work"));
-        ResIterator instanceit = mod.listResourcesWithProperty(RDF.type, mod.getResource("http://purl.bdrc.io/ontology/core/Instance"));
-        while (instanceit.hasNext()) {
-            Resource instance = instanceit.next();
-            isInstance.put(instance, true);
+        final StmtIterator mainIt = mod.listLiteralStatements(null, mod.getProperty("http://purl.bdrc.io/ontology/tmp/isRoot"), true);
+        while (mainIt.hasNext()) {
+            final Resource mainRes = mainIt.removeNext().getSubject();
+            isInstance.put(mainRes, true);
         }
         if (log.isDebugEnabled())
             log.debug("InstanceResults.getResultMap(), checkpoint1: {}", (System.nanoTime()-start)/1000);
         StmtIterator allIterator = mod.listStatements();
         while (allIterator.hasNext()) {
-            Statement st = allIterator.next();
+            final Statement st = allIterator.next();
             
             final Resource subject = st.getSubject();
             
