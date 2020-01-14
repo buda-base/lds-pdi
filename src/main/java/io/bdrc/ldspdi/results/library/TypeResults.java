4510d7763c392674f5a61acbfa0014a8060cfc77
eroux <elie.roux@telecom-bretagne.eu>
2020-01-14 15:27:40 +0100

    attempt to fix bug

--- src/main/java/io/bdrc/ldspdi/results/library/TypeResults.java
+++ src/main/java/io/bdrc/ldspdi/results/library/TypeResults.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.slf4j.Logger;
@@ -19,21 +21,20 @@ public class TypeResults {
 
     public final static Logger log = LoggerFactory.getLogger(TypeResults.class);
 
+    public final static Property tmpIsMain = ResourceFactory.createProperty("http://purl.bdrc.io/ontology/tmp/isMain");
+    
     public static Map<String, Object> getResultsMap(final Model mod) throws RestException {
         long start = 0;
         if (log.isDebugEnabled())
             start = System.nanoTime();
-       
-        log.error("looking for isMain");
         Map<String, Object> res = new HashMap<>();
         Map<String, ArrayList<Field>> main = new HashMap<>();
         Map<String, ArrayList<Field>> aux = new HashMap<>();
         Map<Resource, Boolean> isMain = new HashMap<>();
-        final StmtIterator mainIt = mod.listLiteralStatements(null, mod.getProperty("http://purl.bdrc.io/ontology/tmp/isMain"), true);
+        final StmtIterator mainIt = mod.listLiteralStatements(null, tmpIsMain, true);
         while (mainIt.hasNext()) {
-            final Resource mainRes = mainIt.removeNext().getSubject();
+            final Resource mainRes = mainIt.next().getSubject();
             isMain.put(mainRes, true);
-            log.error("ismain: "+mainRes.getLocalName());
         }
         if (log.isDebugEnabled())
             log.debug("InstanceResults.getResultMap(), checkpoint1: {}", (System.nanoTime()-start)/1000);
@@ -41,13 +42,12 @@ public class TypeResults {
         while (allIterator.hasNext()) {
             final Statement st = allIterator.next();
             
+            if (st.getPredicate().equals(tmpIsMain))
+                continue;
+            
             final Resource subject = st.getSubject();
-            
-            final Boolean subjectIsMain = isMain.getOrDefault(subject, false);
-            log.error("subject: "+subject.getLocalName()+" is main: "+subjectIsMain);
-            
             List<Field> stlist;
-            if (subjectIsMain) {
+            if (isMain.getOrDefault(subject, false)) {
                 stlist = main.computeIfAbsent(subject.getURI(), x -> new ArrayList<Field>());
             } else {
                 stlist = aux.computeIfAbsent(subject.getURI(), x -> new ArrayList<Field>());
