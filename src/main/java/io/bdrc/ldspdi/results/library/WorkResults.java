1409a31cb1f273caa22d0389e16220b6f11d1dba
eroux <elie.roux@telecom-bretagne.eu>
2020-05-12 11:05:33 +0200

    use isMain in taxonomy results

--- src/main/java/io/bdrc/ldspdi/results/library/WorkResults.java
+++ src/main/java/io/bdrc/ldspdi/results/library/WorkResults.java
@@ -7,11 +7,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
-import org.apache.jena.vocabulary.RDF;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +23,8 @@ import io.bdrc.taxonomy.Taxonomy;
 public class WorkResults {
 
     public final static Logger log = LoggerFactory.getLogger(WorkResults.class);
+    
+    public final static Property tmpIsMain = ResourceFactory.createProperty("http://purl.bdrc.io/ontology/tmp/isMain");
 
     public static Map<String, Object> getResultsMap(Model mod) throws RestException {
         long start = 0;
@@ -35,34 +38,39 @@ public class WorkResults {
         Map<String, HashSet<String>> WorkBranch = new HashMap<>();
         Map<String, Object> facets = new HashMap<>();
         HashSet<String> tops = new HashSet<>();
-        Map<Resource, Boolean> isWork = new HashMap<>();
-        ResIterator workit = mod.listResourcesWithProperty(RDF.type);
-        while (workit.hasNext()) {
-            Resource work = workit.next();
-            isWork.put(work, true);
+        Map<Resource, Boolean> isMain = new HashMap<>();
+        ResIterator mainit = mod.listResourcesWithProperty(tmpIsMain);
+        while (mainit.hasNext()) {
+            Resource mainR = mainit.next();
+            isMain.put(mainR, true);
         }
         if (log.isDebugEnabled())
             log.debug("WorkResults.getResultMap(), checkpoint1: {}", (System.nanoTime() - start) / 1000);
         StmtIterator allIterator = mod.listStatements();
         while (allIterator.hasNext()) {
             Statement st = allIterator.next();
+            
+            if (st.getPredicate().equals(tmpIsMain))
+                continue;
 
             final Resource subject = st.getSubject();
 
-            final Boolean subjectIsWork = isWork.getOrDefault(subject, false);
+            final Boolean subjectIsMain = isMain.getOrDefault(subject, false);
 
             List<Field> stlist;
-            if (subjectIsWork) {
+            if (subjectIsMain) {
                 stlist = main.computeIfAbsent(subject.getURI(), x -> new ArrayList<Field>());
             } else {
                 stlist = aux.computeIfAbsent(subject.getURI(), x -> new ArrayList<Field>());
             }
             stlist.add(Field.getField(st));
-
-            String prop = st.getPredicate().getURI();
-            if (prop.equals(Taxonomy.WORK_GENRE)
-                    || prop.equals(Taxonomy.WORK_IS_ABOUT) && st.getObject().asResource().getLocalName().startsWith("T")) {
-                Taxonomy.processTopicStatement(st, tops, Wtopics, WorkBranch, topics);
+            
+            if (subjectIsMain) {
+                String prop = st.getPredicate().getURI();
+                if (prop.equals(Taxonomy.WORK_GENRE)
+                        || prop.equals(Taxonomy.WORK_IS_ABOUT) && st.getObject().asResource().getLocalName().startsWith("T")) {
+                    Taxonomy.processTopicStatement(st, tops, Wtopics, WorkBranch, topics);
+                }
             }
         }
         res.put("main", main);
