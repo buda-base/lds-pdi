83bf82eaa2280fd203f099a915856ffcc463330b
eroux <elie.roux@telecom-bretagne.eu>
2021-06-16 19:10:56 +0200

    add temporary endpoint for Zotero debugging

--- src/main/java/io/bdrc/ldspdi/rest/controllers/unAPIController.java
+++ src/main/java/io/bdrc/ldspdi/rest/controllers/unAPIController.java
@@ -38,5 +38,32 @@ public class unAPIController {
         }
         return ResponseEntity.status(302).header("Location", redirect).build();
     }
+
+    @GetMapping(value = "UNAPI300", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<String> commonEndPoint300(@RequestParam(value="id", required=false) String id, @RequestParam(value="format", required=false) String format) {
+        if (id == null || id.isEmpty())
+            return ResponseEntity.ok().body("<?xml version=\"1.0\" encoding=\"UTF-8\"?><formats></formats>");
+        String shortId = id;
+        if (id.startsWith("http://purl.bdrc.io/resource/")) {
+            shortId = "bdr:"+id.substring(29);
+        }
+        if (!shortId.startsWith("bdr:MW") && (!shortId.startsWith("bdr:W") || shortId.startsWith("bdr:WA"))) {
+            return ResponseEntity.status(404).body("<?xml version=\"1.0\" encoding=\"UTF-8\"?><formats id=\""+id+"\"></formats>");
+        }
+        if (format == null || format.isEmpty()) {
+            // specs says HTTP status 300 but:
+            // https://github.com/zotero/translators/issues/2459
+            return ResponseEntity.status(300).body("<?xml version=\"1.0\" encoding=\"UTF-8\"?><formats id=\""+id+"\"><format name=\"marcxml\" type=\"application/marcxml+xml\" /></formats>");
+        }
+        if (!format.equals("marcxml"))
+            return ResponseEntity.status(406).body("format not available");
+        String redirect;
+        if (id.startsWith("bdr:")) {
+            redirect = "https://purl.bdrc.io/resource/"+id.substring(4)+".mrcx"; 
+        } else {
+            redirect = "https"+id.substring(4)+".mrcx";
+        }
+        return ResponseEntity.status(302).header("Location", redirect).build();
+    }
     
 }
