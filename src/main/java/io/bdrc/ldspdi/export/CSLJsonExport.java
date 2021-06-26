d7ccb7aa3e0d4c9dd7cc5600dbf0af756a1e0606
eroux <elie.roux@telecom-bretagne.eu>
2021-06-26 20:36:26 +0200

    CSL WIP

---
+++ src/main/java/io/bdrc/ldspdi/export/CSLJsonExport.java
@@ -0,0 +1,326 @@
+package io.bdrc.ldspdi.export;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.vocabulary.OWL;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.SKOS;
+import org.marc4j.MarcException;
+import org.marc4j.MarcStreamWriter;
+import org.marc4j.MarcWriter;
+import org.marc4j.MarcXmlWriter;
+import org.marc4j.marc.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import io.bdrc.ewtsconverter.EwtsConverter;
+import io.bdrc.ewtsconverter.TransConverter;
+import io.bdrc.ldspdi.exceptions.LdsError;
+import io.bdrc.ldspdi.exceptions.RestException;
+import io.bdrc.ldspdi.sparql.QueryProcessor;
+import io.bdrc.libraries.BudaMediaTypes;
+
+/*
+ * Here's some knowledge that can be useful when dealing with libraries:
+ *
+ * https://aurimasv.github.io/z2csl/typeMap.xml
+ * 
+ * we use two types:
+ *   book
+ *   bookSection
+ */
+
+public class CSLJsonExport {
+
+    public final static Logger log = LoggerFactory.getLogger(CSLJsonExport.class);
+    
+    public static final EwtsConverter ewtsConverter = new EwtsConverter();
+    
+    public static final String ewtsToBo(String ewts) {
+        if (ewts.startsWith("*"))
+            ewts = ewts.substring(1);
+        String unicode = ewtsConverter.toUnicode(ewts);
+        return unicode;
+    }
+    
+    public static final String ewtsToLatn(String ewts) {
+        if (ewts.startsWith("*"))
+            ewts = ewts.substring(1);
+        String alalc = TransConverter.ewtsToAlalc(ewts, true);
+        alalc = alalc.replace("u0fbe", "x");
+        return StringUtils.capitalize(alalc.replace('-', ' ').trim());
+    }
+    
+    public static final class FieldInfo {
+        public String label_latn = null;
+        public String label_bo = null;
+        public String label_bo_latn = null;
+        public String label_en = null;
+        public String label_sa_tibt = null;
+        public String label_sa_tibt_latn = null;
+        public String label_sa_latn = null;
+        public String label_sa_latn_ndia = null;
+        public String label_zh = null;
+        public String label_zh_pinyin = null;
+
+        public FieldInfo() {}
+        
+        public void addFromLiteral(final Literal l, final boolean replaceIfPresent) {
+            final String lang = l.getLanguage();
+            switch(lang) {
+            case "bo":
+            case "dz":
+                if (replaceIfPresent || this.label_bo == null)
+                    this.label_bo = l.getString();
+                break;
+            case "bo-x-ewts":
+            case "dz-x-ewts":
+                if (replaceIfPresent || this.label_bo_latn == null)
+                    this.label_bo_latn = ewtsToLatn(l.getString());
+                if (replaceIfPresent || this.label_bo == null)
+                    this.label_bo = ewtsToBo(l.getString());
+                break;
+            case "sa-x-ewts":
+                if (replaceIfPresent || this.label_sa_tibt_latn == null)
+                    this.label_sa_tibt_latn = ewtsToLatn(l.getString());
+                if (replaceIfPresent || this.label_bo == null)
+                    this.label_sa_tibt = ewtsToBo(l.getString());
+                break;
+            case "bo-alalc97":
+                if (replaceIfPresent || this.label_bo_latn == null)
+                    this.label_bo_latn = StringUtils.capitalize(l.getString());
+            case "sa-alalc97":
+                if (replaceIfPresent || this.label_sa_latn == null)
+                    this.label_sa_latn = StringUtils.capitalize(l.getString());
+            case "sa-x-iast":
+                if (replaceIfPresent || this.label_sa_latn == null)
+                    this.label_sa_latn = StringUtils.capitalize(l.getString());
+            case "sa-x-ndia":
+                if (replaceIfPresent || this.label_sa_latn_ndia == null)
+                    this.label_sa_latn_ndia = StringUtils.capitalize(l.getString());
+            case "zh-hans":
+                if (replaceIfPresent || this.label_zh == null)
+                    this.label_zh = StringUtils.capitalize(l.getString());
+            case "zh-latn-pinyin":
+                if (replaceIfPresent || this.label_zh_pinyin == null)
+                    this.label_zh_pinyin = StringUtils.capitalize(l.getString());
+            case "en":
+                if (replaceIfPresent || this.label_en == null)
+                    this.label_en = StringUtils.capitalize(l.getString());
+            default:
+                log.debug("ignoring lang tag "+lang);
+            }
+        }
+        
+        public void fill_missing() {
+            // the three fields we're going to use are:
+            // - bo
+            // - bo_latn
+            // - en
+            // - zh
+            // let's fill bo first:
+            fill_missing_bo();
+            fill_missing_latn();
+            fill_missing_en();
+            fill_missing_zh();
+        }
+        
+        public void fill_missing_bo() {
+            if (this.label_bo != null)
+                return;
+            if (this.label_sa_tibt != null) {
+                this.label_bo = this.label_sa_tibt;
+                return;
+            }
+            if (this.label_zh != null) {
+                this.label_bo = this.label_sa_tibt;
+                return;
+            }
+            if (this.label_sa_latn != null) {
+                this.label_bo = this.label_sa_latn;
+                return;
+            }
+            if (this.label_sa_latn_ndia != null) {
+                this.label_bo = this.label_sa_latn_ndia;
+                return;
+            }
+            if (this.label_en != null) {
+                this.label_bo = this.label_en;
+                return;
+            }
+        }
+        
+        public void fill_missing_latn() {
+            if (this.label_latn != null)
+                return;
+            if (this.label_sa_latn != null) {
+                this.label_latn = this.label_sa_latn;
+                return;
+            }
+            if (this.label_sa_latn_ndia != null) {
+                this.label_latn = this.label_sa_latn_ndia;
+                return;
+            }
+            if (this.label_bo_latn != null) {
+                this.label_latn = this.label_bo_latn;
+                return;
+            }
+            if (this.label_sa_tibt_latn != null) {
+                this.label_latn = this.label_sa_tibt_latn;
+                return;
+            }
+            if (this.label_zh_pinyin != null) {
+                this.label_latn = this.label_zh_pinyin;
+                return;
+            }
+            if (this.label_en != null) {
+                this.label_latn = this.label_en;
+                return;
+            }
+            if (this.label_zh != null) {
+                this.label_latn = this.label_zh;
+                return;
+            }
+        }
+
+        public void fill_missing_en() {
+            if (this.label_en != null)
+                return;
+            this.label_en = this.label_latn;
+        }
+        
+        public void fill_missing_zh() {
+            if (this.label_zh != null)
+                return;
+            if (this.label_zh_pinyin != null) {
+                this.label_zh = this.label_zh_pinyin;
+                return;
+            }
+            if (this.label_sa_latn != null) {
+                this.label_zh = this.label_sa_latn;
+                return;
+            }
+            if (this.label_bo != null) {
+                this.label_zh = this.label_bo;
+                return;
+            }
+            if (this.label_sa_tibt != null) {
+                this.label_zh = this.label_sa_tibt;
+                return;
+            }
+            if (this.label_en != null) {
+                this.label_zh = this.label_en;
+                return;
+            }
+            if (this.label_latn != null) {
+                this.label_zh = this.label_latn;
+                return;
+            }
+        }
+        
+    }
+    
+    public static final FieldInfo getEntityLabelField(Model m, Resource r, final boolean followSameAs) {
+        FieldInfo fi = new FieldInfo();
+        addEntityLabels(m, r, fi);
+        if (followSameAs) {
+            NodeIterator si = m.listObjectsOfProperty(r, OWL.sameAs);
+            while (si.hasNext()) {
+                Resource sa = si.next().asResource();
+                addEntityLabels(m, sa, fi);
+            }
+        }
+        return fi;
+    }
+    
+    public static final void addEntityLabels(Model m, Resource r, final FieldInfo fi) {
+        NodeIterator si = m.listObjectsOfProperty(r, SKOS.prefLabel);
+        while (si.hasNext()) {
+            Literal l = si.next().asLiteral();
+            fi.addFromLiteral(l, false);
+        }
+    }
+    
+    public static final FieldInfo getLabelFieldForLiteral(Literal l) {
+        FieldInfo fi = new FieldInfo();
+        fi.addFromLiteral(l, false);
+        fi.fill_missing();
+        return fi;
+    }
+
+    public static Model getModelForCSL(final String resUri) throws RestException {
+        Model model = QueryProcessor.getSimpleResourceGraph(resUri, "resForCSL.arq");
+        if (model.size() < 1) {
+            throw new RestException(404, new LdsError(LdsError.NO_GRAPH_ERR).setContext(resUri));
+        }
+        final Resource main = model.getResource(resUri);
+        final StmtIterator stmti = main.listProperties(RDF.type);
+        boolean isInstance = false;
+        while (stmti.hasNext()) {
+            final Resource type = stmti.next().getObject().asResource();
+            if (type.getLocalName().contains("Instance")) {
+                isInstance = true;
+                break;
+            }
+        }
+        if (!isInstance) {
+            throw new RestException(404, new LdsError(LdsError.NO_GRAPH_ERR).setMsg("Resource is not an Instance"));
+        }
+        return model;
+    }
+    
+    public static class CSLResObj {
+        @JsonIgnore
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode bo;
+        ObjectNode latn;
+        ObjectNode en;
+        ObjectNode zh;
+        
+        CSLResObj() {
+            this.bo = mapper.createObjectNode();
+            this.latn = mapper.createObjectNode();
+            this.en = mapper.createObjectNode();
+            this.zh = mapper.createObjectNode();
+        }
+    }
+    
+    public static ResponseEntity<CSLResObj> getResponse(final String resUri) throws RestException, JsonProcessingException {
+        // I really don't like that but making that better would mean either:
+        // - a very weird and probably slower SPARQL query
+        // - two queries
+        // The idea is that we're sending MARC records for items to Columbia,
+        // as they only want "electronic resources" records, and electronic
+        // resources are imageinstances (scans) in our system, not regular instances.
+        boolean scansMode = resUri.startsWith(MarcExport.ScanUriPrefix);
+        final Model m;
+        final Resource main;
+
+        m = getModelForCSL(resUri);
+        main = m.getResource(resUri);
+        
+        CSLResObj res = new CSLResObj();
+        
+        // add stuff
+        
+        return ResponseEntity.ok().header("Allow", "GET, OPTIONS, HEAD").header("Vary", "Negotiate, Accept").contentType(MediaType.APPLICATION_JSON).body(res);
+
+    }
+    
+}
---
+++ src/test/java/io/bdrc/ldspdi/test/CSLExportTest.java
@@ -0,0 +1,83 @@
+package io.bdrc.ldspdi.test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.jena.fuseki.main.FusekiServer;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import io.bdrc.ldspdi.rest.controllers.PublicDataController;
+import io.bdrc.ldspdi.service.ServiceConfig;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { PublicDataController.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration
+public class CSLExportTest {
+
+    @Autowired
+    Environment environment;
+
+    private static FusekiServer server;
+    private static Dataset srvds = DatasetFactory.createTxnMem();
+    private static Model model = ModelFactory.createDefaultModel();
+    public static String fusekiUrl;
+    public final static Logger log = LoggerFactory.getLogger(MarcExportTest.class.getName());
+
+    @BeforeClass
+    public static void init() throws JsonParseException, JsonMappingException, IOException {
+        fusekiUrl = "http://localhost:2251/bdrcrw";
+        ServiceConfig.initForTests(fusekiUrl);
+        Utils.loadDataInModel(model);
+        srvds.setDefaultModel(model);
+        // try {
+        // OntData.init();
+        // } catch (RestException e) {
+        // e.printStackTrace();
+        // }
+        // Creating a fuseki server
+        server = FusekiServer.create().port(2251).add("/bdrcrw", srvds).build();
+        server.start();
+    }
+
+    @AfterClass
+    public static void close() {
+        server.stop();
+        server.join();
+    }
+
+    @Test
+    public void testSimpleRequestSimple() throws ClientProtocolException, IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet("http://localhost:" + environment.getProperty("local.server.port") + "/resource/W23819.mrcx");
+        HttpResponse response = client.execute(get);
+        System.out.println(response.getStatusLine().getStatusCode());
+        System.out.println("result:");
+        //response.getEntity().writeTo(System.out);
+        try (FileOutputStream fos = new FileOutputStream("/tmp/marc.xml")) {
+            response.getEntity().writeTo(fos);
+         }
+    }
+
+}
